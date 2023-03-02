1579a685c2a10ebe198fe7c2924437fa2d6fe485
lucia <luciatiradoturiel@gmail.com>
2023-03-02 15:05:58 +0100

    b

--- src/main/java/Connection/ConnectionMySQL.java
+++ src/main/java/Connection/ConnectionMySQL.java
@@ -1,15 +1,14 @@
 package Connection;
 import javax.swing.*;
 import java.sql.Connection;
-import java.sql.DriverManager;
 
 
 public class ConnectionMySQL {
     public static Connection getConnection(){
         Connection con = null;
         String name = "root";
-        String url = "jdbc:mysql://localhost:3306/taskdatabase";
-        String psw = "";
+        String url = "jdbc:mysql://localhost:3306/ToDoList";
+        String psw = "10021002";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
